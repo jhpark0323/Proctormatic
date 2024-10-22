29591dfbe3e4153ac539aa7b566cb12cfcd75a27
heo-yujeong <yu.jeong5899@gmail.com>
2024-10-23 01:30:52 +0900

    [#S11P31S209-74] [BE] chore: settings.py에 앱 및 패키지 등록, 기본 user model로 변경

--- backend/proctormatic/settings.py
+++ backend/proctormatic/settings.py
@@ -31,6 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # APP
+    'accounts',
+
+    # DRF
+    'rest_framework',
+    'rest_framework.authtoken',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -125,3 +132,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.User'
