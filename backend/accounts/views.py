73e8cb2246e695ef8c17ae718f8b70b1100f8c14
heo-yujeong <yu.jeong5899@gmail.com>
2024-10-23 23:10:28 +0900

    [#S11P31S209-80] [BE] feat: 인증번호 발송 및 인증 기능

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,11 +1,12 @@
+import re
 from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.response import Response
-from django.core.validators import validate_email
-from django.core.exceptions import ValidationError
 from django.contrib.auth import get_user_model
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
+from django_redis import get_redis_connection
+from .utils import generate_verification_code, send_verification_email, save_verification_code_to_redis
 
 User = get_user_model()
 
@@ -34,9 +35,7 @@ def chech_email_exists(request):
     if request.method == 'GET':
         email = request.query_params.get('email')
 
-        try:
-            validate_email(email)
-        except ValidationError:
+        if not is_valid_email(email):
             return Response({'message': '이메일 형식을 확인해주세요.'}, status=status.HTTP_400_BAD_REQUEST)
 
         if email:
@@ -47,3 +46,96 @@ def chech_email_exists(request):
                 return Response({'isAlreadyExists': email_exists}, status=status.HTTP_200_OK)
         else:
             return Response({'message': '이메일을 입력해주세요.'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@swagger_auto_schema(
+    method='post',
+    request_body=openapi.Schema(
+        type=openapi.TYPE_OBJECT,
+        properties={
+            'email': openapi.Schema(type=openapi.TYPE_STRING),
+        }
+    ),
+    responses={
+        200: openapi.Response('인증번호를 발송했습니다.', openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'message': openapi.Schema(type=openapi.TYPE_STRING),
+            }
+        )),
+        400: openapi.Response('잘못된 이메일 형식 또는 이메일 미제공', openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'message': openapi.Schema(type=openapi.TYPE_STRING),
+            }
+        )),
+    }
+)
+@swagger_auto_schema(
+    method='put',
+    request_body=openapi.Schema(
+        type=openapi.TYPE_OBJECT,
+        properties={
+            'email': openapi.Schema(type=openapi.TYPE_STRING),
+            'code': openapi.Schema(type=openapi.TYPE_STRING),
+        }
+    ),
+    responses={
+        200: openapi.Response('이메일 인증이 완료되었습니다.', openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'message': openapi.Schema(type=openapi.TYPE_STRING),
+            }
+        )),
+        400: openapi.Response('잘못된 인증번호 또는 만료된 인증번호', openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'message': openapi.Schema(type=openapi.TYPE_STRING),
+            }
+        )),
+    }
+)
+@api_view(['POST', 'PUT'])
+def handle_email_verification(request):
+    email = request.data.get('email')
+
+    if request.method == 'POST':
+        if email:
+            if not is_valid_email(email):
+                return Response({'message': '이메일 형식을 확인해주세요.'}, status=status.HTTP_400_BAD_REQUEST)
+
+            code = generate_verification_code()
+            send_verification_email(email, code)
+            save_verification_code_to_redis(email, code)
+
+            return Response({'message': '인증번호를 발송했습니다.'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'message': '이메일을 입력해주세요.'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+    elif request.method == 'PUT':
+        code = request.data.get('code')
+
+        if not email:
+            return Response({'message': '이메일을 입력해주세요.'}, status=status.HTTP_400_BAD_REQUEST)
+        if not code:
+            return Response({'message': '인증번호를 입력해주세요.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        redis_conn = get_redis_connection('default')
+        stored_code = redis_conn.get(f'verification_code:{email}')
+
+        if stored_code is None:
+            return Response({'message': '인증번호가 만료되었습니다.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        if stored_code.decode('utf-8') == code:
+            return Response({'message': '이메일 인증이 완료되었습니다.'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'message': '잘못된 인증번호입니다.'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+def is_valid_email(email):
+    email_regex = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+
+    if re.match(email_regex, email):
+        return True
+    return False
