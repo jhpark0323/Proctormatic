bb783374d46f9f557b05ed79ae1facf38ca32742
윤예리 <yoonr721@gmail.com>
2024-10-23 10:35:57 +0900

    [#S11P31S209-153] [FE] feat: 상수 추가 - colors.ts : 색상 - fonts.ts : 글자 크기 - shadows.ts : box-shadow

---
+++ frontend/src/constants/fonts.ts
@@ -0,0 +1,33 @@
+const fonts = {
+  // Heading
+  HEADING_4XL: "bold 48px/140% Pretendard",
+  HEADING_3XL: "bold 40px/140% Pretendard",
+  HEADING_2XL: "semibold 32px/140% Pretendard",
+  HEADING_1XL: "semibold 28px/140% Pretendard",
+  HEADING_LG: "semibold 24px/140% Pretendard",
+  HEADING_MD: "semibold 20px/140% Pretendard",
+  HEADING_SM: "semibold 18px/140% Pretendard",
+
+  // Body
+  LG_SEMIBOLD: "semibold 16px/150% Pretendard",
+  LG_MEDIUM: "medium 16px/150% Pretendard",
+  LG_REGULAR: "regular 16px/150% Pretendard",
+  LG_ITALIC: "italic light 16px/150% Pretendard",
+
+  MD_SEMIBOLD: "semibold 14px/150% Pretendard",
+  MD_MEDIUM: "medium 14px/150% Pretendard",
+  MD_REGULAR: "regular 14px/150% Pretendard",
+  MD_ITALIC: "italic light 14px/150% Pretendard",
+
+  SM_SEMIBOLD: "semibold 12px/160% Pretendard",
+  SM_MEDIUM: "medium 12px/160% Pretendard",
+  SM_REGULAR: "regular 12px/160% Pretendard",
+  SM_ITALIC: "italic light 12px/160% Pretendard",
+
+  XSM_SEMIBOLD: "semibold 10px/160% Pretendard",
+  XSM_MEDIUM: "medium 10px/160% Pretendard",
+  XSM_REGULAR: "regular 10px/160% Pretendard",
+  XSM_ITALIC: "italic light 10px/160% Pretendard",
+};
+
+export { fonts };
---
+++ frontend/src/constants/shadows.ts
@@ -0,0 +1,21 @@
+const shadows = {
+  // Light
+  LIGHT_1: "0px 2px 4px 0px rgba(0, 0, 0, 0.1)",
+  LIGHT_2:
+    "0px 1px 4px 0px rgba(0, 0, 0, 0.12), 0px 2px 8px 0px rgba(0, 0, 0, 0.14)",
+  LIGHT_3:
+    "0px 1px 6px 0px rgba(0, 0, 0, 0.12), 0px 4px 12px 0px rgba(0, 0, 0, 0.20)",
+  LIGHT_4:
+    "0px 1px 8px 0px rgba(0, 0, 0, 0.12), 0px 8px 20px 0px rgba(0, 0, 0, 0.20)",
+
+  // Dark
+  DARK_1: "0px 2px 4px 0px rgba(0, 0, 0, 0.3)",
+  DARK_2:
+    "0px 1px 4px 0px rgba(0, 0, 0, 0.24), 0px 2px 8px 0px rgba(0, 0, 0, 0.26)",
+  DARK_3:
+    "0px 1px 6px 0px rgba(0, 0, 0, 0.26), 0px 4px 12px 0px rgba(0, 0, 0, 0.28)",
+  DARK_4:
+    "0px 1px 16px 0px rgba(0, 0, 0, 0.28), 0px 8px 20px 0px rgba(0, 0, 0, 0.32)",
+};
+
+export { shadows };
