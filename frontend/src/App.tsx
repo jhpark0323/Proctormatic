384d749d76fc0294b3b905d5624a8e283c6b67be
윤예리 <yoonr721@gmail.com>
2024-10-23 10:58:56 +0900

    [#S11P31S209-153] [FE] HOTFIX: App.tsx 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import TakerHome from "./pages/taker/TakerHome";
 import HostHome from "./pages/host/HostHome";
 import { useAuthStore } from "./store/useAuthStore";
 import PrivateRoute from "./components/PrivateRoute";
+import "./App.css";
 
 function App() {
   // const fetchUser = useAuthStore((state) => state.fetchUser);
