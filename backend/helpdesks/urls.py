1cde7a6f039bb0e3d5515cd52e70ff9534f9ae6e
이승민 <lsm5631@naver.com>
2024-10-24 15:05:47 +0900

    [#S11P31S209-148] [BE] feat: 공지사항 등록

--- backend/helpdesks/urls.py
+++ backend/helpdesks/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('notification/', views.create_notification, name='create_notification')
+    path('notification/', views.create_notification)
 ]
