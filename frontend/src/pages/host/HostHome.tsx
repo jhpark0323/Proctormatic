d6e5b654e86e3edc939fad8be1470eadbdf2ae20
윤예리 <yoonr721@gmail.com>
2024-10-25 16:21:02 +0900

    [#S11P31S209-158] [FE] :sparkles: feat: 토글 버튼

--- frontend/src/pages/host/HostHome.tsx
+++ frontend/src/pages/host/HostHome.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React from "react";
+import ToggleSwitch from "@/components/ToggleSwitch";
+import Checkbox from "@/components/Checkbox";
+import Textfield from "@/components/Textfield";
 
 const HostHome = () => {
+  const [checkbox, toggleSwitch] = React.useState(false);
   return (
     <>
+      {/* <ToggleSwitch>하이</ToggleSwitch>
+      <Checkbox checked={checkbox} onChange={toggleSwitch}>
+        하이
+      </Checkbox> */}
+      <Textfield></Textfield>
       <div>
         <h2>주최자 페이지</h2>
         <p>여기는 주최자를 위한 페이지입니다.</p>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default HostHome;
