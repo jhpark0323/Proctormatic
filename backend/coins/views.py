b5283e93ae711e22ecac79173326e7bb0b84c2a3
허유정 <yu.jeong5899@gmail.com>
2024-10-25 17:48:00 +0900

    [#S11P31S209-179] [BE] feat: 적립금 조회 기능

--- backend/coins/views.py
+++ backend/coins/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from django.contrib.auth import get_user_model
+from rest_framework import status
+from rest_framework.response import Response
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
-# Create your views here.
+User = get_user_model()
+
+@swagger_auto_schema(
+    method='get',
+    operation_summary="적립금 조회",
+    manual_parameters=[
+        openapi.Parameter('Authorization', openapi.IN_HEADER, type=openapi.TYPE_STRING)
+    ],
+    responses={
+        200: openapi.Response('적립금 조회 성공', openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'isAlreadyExists': openapi.Schema(type=openapi.TYPE_BOOLEAN),
+            }
+        ))
+    }
+)
+@api_view(['GET'])
+def handle_coin(request):
+    if request.method == 'GET':
+        user_id = request.auth['id']
+        user = User.objects.get(pk=user_id)
+        return Response({'coin': user.coin_amount}, status=status.HTTP_200_OK)
