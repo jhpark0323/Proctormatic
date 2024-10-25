1b29b89929f1c5260a2cfdafec75b5797570057c
jhpark <brianwogus@naver.com>
2024-10-25 17:15:53 +0900

    [#S11P31S209-126] [BE] feat: 진행중인 시험 조회

--- backend/exams/serializers.py
+++ backend/exams/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Exam
+from takers.models import Taker
 
 class ExamSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,3 +12,14 @@ class ScheduledExamListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Exam
         fields = ['id', 'title', 'date', 'start_time', 'end_time', 'url', 'expected_taker']
+
+class OngoingExamListSerializer(serializers.ModelSerializer):
+    total_taker = serializers.SerializerMethodField()  # 동적으로 계산할 필드
+
+    class Meta:
+        model = Exam
+        fields = ['id', 'title', 'date', 'start_time', 'end_time', 'url', 'expected_taker', 'total_taker']
+
+    def get_total_taker(self, obj):
+        # Taker 모델에서 특정 시험에 응시한 사람들의 수를 카운트
+        return Taker.objects.filter(exam_id=obj.id).count()
--- backend/exams/urls.py
+++ backend/exams/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import create_exam, scheduled_exam_list
+from .views import create_exam, scheduled_exam_list, ongoing_exam_list
 
 urlpatterns = [
     path('', create_exam),
     path('scheduled/', scheduled_exam_list),
+    path('ongoing/', ongoing_exam_list),
 ]
