fe83de990424b3c0a49e309eb115e81ddd47abeb
허유정 <yu.jeong5899@gmail.com>
2024-10-25 17:47:00 +0900

    [#S11P31S209-179] [BE] feat: 적립금 model 생성

--- backend/coins/models.py
+++ backend/coins/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+from exams.models import Exam
+
+
+class Coin(models.Model):
+    TYPE_CHOICES = (
+        ('charge', 'Charge'),
+        ('use', 'Use'),
+        ('refund', 'Refund'),
+    )
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    exam = models.ForeignKey(Exam, null=True, blank=True, on_delete=models.CASCADE)
+    type = models.CharField(max_length=20, choices=TYPE_CHOICES)
+    amount = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
