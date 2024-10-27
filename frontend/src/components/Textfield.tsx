2f9b691cd5d8800ee7d1b6e43b02a262db4f2ebe
윤예리 <yoonr721@gmail.com>
2024-10-27 19:30:38 +0900

    [#S11P31S209-158] [FE] ✨ feat: 텍스트 필드

--- frontend/src/components/Textfield.tsx
+++ frontend/src/components/Textfield.tsx
@@ -1,12 +1,32 @@
 import styles from "@/styles/Buttons.module.css";
+import cancleButton from "@/assets/cancleButton.png";
+import { useState } from "react";
+
+// 사용 예시
+/* 
+  const [name, setName] = useState("윤예리");
+
+      <Textfield
+        label="이름"
+        helpMessage="이름을 입력하세요"
+        placeholder="홍길동"
+        maxLength=10
+        trailingIcon={true}
+        onChange={(value) => setName(value)}
+      >
+        {name}
+      </Textfield>
+*/
 
 interface TextfieldProps {
   label?: string;
   helpMessage?: string;
   placeholder?: string;
-  trailingIcon?: string;
+  trailingIcon?: boolean;
+  type?: "underline" | "default";
+  maxLength?: number;
   children?: React.ReactNode;
-  type: "underline" | "default";
+  onChange?: (value: string) => void;
 }
 
 const Textfield = ({
@@ -14,9 +34,23 @@ const Textfield = ({
   helpMessage,
   placeholder,
   trailingIcon,
+  maxLength,
   children = "",
   type = "default",
+  onChange,
 }: TextfieldProps) => {
+  const [inputValue, setInputValue] = useState(children?.toString() || "");
+  const handleClear = () => {
+    setInputValue(""); // 입력 필드 초기화
+  };
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value;
+    setInputValue(newValue);
+    if (onChange) {
+      onChange(newValue); // 값 변경 시 부모 컴포넌트로 전달
+    }
+  };
+
   return (
     <div className={styles["textfield-container"]}>
       {/* 라벨 */}
@@ -27,6 +61,9 @@ const Textfield = ({
         <input
           type="text"
           placeholder={placeholder}
+          value={inputValue}
+          onChange={handleChange}
+          maxLength={maxLength}
           defaultValue={children?.toString()} // defaultValue로 설정
           className={`${styles["textfield-input"]} ${
             type === "underline" ? styles["underline"] : ""
@@ -36,9 +73,11 @@ const Textfield = ({
         {/* 트레일링 아이콘 */}
         {trailingIcon && (
           <img
-            src={trailingIcon}
+            src={cancleButton}
             alt="icon"
             className={styles["trailing-icon"]}
+            onClick={handleClear} // 아이콘 클릭 시 내용 지우기
+            style={{ cursor: "pointer" }} // 커서 스타일 변경
           />
         )}
       </div>
