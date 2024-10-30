41332459b8dbcbfbab3be61e080cbcd2aac362f4
이승민 <lsm5631@naver.com>
2024-10-30 10:41:33 +0900

    hotfix: swagger urls 세팅 변경

--- backend/proctormatic/urls.py
+++ backend/proctormatic/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
+from django.urls import re_path
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -34,6 +35,8 @@ urlpatterns = [
     path('api/helpdesk/', include('helpdesks.urls')),
     path('api/taker/', include('takers.urls')),
     path('api/swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 if settings.DEBUG:
