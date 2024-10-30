ac92c03e771d75473aad0e88e694f420bde3af71
이승민 <lsm5631@naver.com>
2024-10-30 11:01:58 +0900

    hotfix: setting.py 수정

--- backend/proctormatic/settings.py
+++ backend/proctormatic/settings.py
@@ -74,7 +74,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware'
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -99,8 +99,6 @@ CORS_ALLOW_CREDENTIALS = True
 
 # 위험사항
 CORS_ALLOW_ALL_ORIGINS = True
-
-CORS_REPLACE_HTTPS_REFERER = True
 
 SWAGGER_SETTINGS = {
     'USE_SESSION_AUTH': False,
