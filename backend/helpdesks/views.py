a52b78f79e175c2498f913634041a0b3a3e3cb55
허유정 <yu.jeong5899@gmail.com>
2024-11-03 16:26:55 +0900

    [#S11P31S209-314] [BE] refactor: swagger 파일 분리

--- backend/helpdesks/views.py
+++ backend/helpdesks/views.py
@@ -1,61 +1,21 @@
-from .models import Notification, Question, Faq
-from .serializers import NotificationCreateSerializer, NotificationListSerializer, NotificationObjectSerializer, \
-    FaqCreateSerializer, FaqListSerializer, FaqSerializer, QuestionSerializer, QuestionEditSerializer, \
-    QuestionCreateSerializer, QuestionListSerializer
 from django.contrib.auth import get_user_model
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny
-from drf_spectacular.utils import extend_schema_view, extend_schema, OpenApiParameter, OpenApiResponse, OpenApiRequest
 from django.core.paginator import Paginator
+
+from .models import Notification, Question, Faq
+from .serializers import NotificationCreateSerializer, NotificationListSerializer, NotificationObjectSerializer, \
+    FaqCreateSerializer, FaqListSerializer, FaqSerializer, QuestionSerializer, QuestionEditSerializer, \
+    QuestionCreateSerializer, QuestionListSerializer
+from .swagger_schemas import notification_schama, check_notification_schema, question_schema, question_detail_schema, \
+    faq_schema, faq_detail_schema
 
 User = get_user_model()
 
 
-@extend_schema_view(
-    get=extend_schema(
-        summary='공지사항 목록 조회',
-        parameters=[
-            OpenApiParameter(name='page', type=int, location=OpenApiParameter.QUERY, default=1),
-            OpenApiParameter(name='size', type=int, location=OpenApiParameter.QUERY, default=10),
-        ],
-        responses={
-            status.HTTP_200_OK: OpenApiResponse(
-                description='공지사항 목록 조회 성공',
-                response=NotificationListSerializer(many=True),
-            ),
-            status.HTTP_400_BAD_REQUEST: OpenApiResponse(
-                description='잘못된 페이지 또는 사이즈 요청',
-                response={
-                    'type': 'object',
-                    'properties': {
-                        'message': {
-                            'type': 'string'
-                        },
-                    },
-                }
-            ),
-        }
-    ),
-    post=extend_schema(
-        summary='공지사항 등록',
-        request=NotificationCreateSerializer,
-        responses={
-            status.HTTP_201_CREATED: OpenApiResponse(
-                description='공지사항 등록 완료',
-                response={
-                    'type': 'object',
-                    'properties': {
-                        'message': {
-                            'type': 'string'
-                        },
-                    }
-                }
-            )
-        }
-    )
-)
+@notification_schama
 @api_view(['GET', 'POST'])
 @permission_classes([AllowAny])
 def notification(request):
@@ -84,45 +44,7 @@ def notification(request):
         return Response(data, status=status.HTTP_200_OK)
 
 
-@extend_schema_view(
-    get=extend_schema(
-        summary='공지사항 조회',
-        responses={
-            status.HTTP_200_OK: OpenApiResponse(
-                description='공지사항 조회 성공',
-                response=NotificationObjectSerializer
-            ),
-            status.HTTP_404_NOT_FOUND: OpenApiResponse(
-                description='공지사항 미존재',
-                response={
-                    'type': 'object',
-                    'properties': {
-                        'message': {
-                            'type': 'string'
-                        },
-                    }
-                }
-            )
-        }
-    ),
-    delete=extend_schema(
-        summary='공지사항 삭제',
-        responses={
-            status.HTTP_204_NO_CONTENT: OpenApiResponse(description='공지사항 삭제 성공'),
-            status.HTTP_404_NOT_FOUND: OpenApiResponse(
-                description='공지사항 미존재',
-                response={
-                    'type': 'object',
-                    'properties': {
-                        'message': {
-                            'type': 'string'
-                        },
-                    }
-                }
-            )
-        }
-    )
-)
+@check_notification_schema
 @api_view(['GET', 'DELETE'])
 @permission_classes([AllowAny])
 def check_notification(request, notification_id):
@@ -139,82 +61,7 @@ def check_notification(request, notification_id):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-@extend_schema_view(
-    get=extend_schema(
-        summary='질문 목록 조회',
-        parameters=[
-            OpenApiParameter(name='page', type=int, location=OpenApiParameter.QUERY, default=1),
-            OpenApiParameter(name='size', type=int, location=OpenApiParameter.QUERY, default=10),
-        ],
-        responses={
-            status.HTTP_200_OK: OpenApiResponse(
-                description='질문 목록 조회 성공',
-                response=QuestionListSerializer(many=True),
-            ),
-            status.HTTP_400_BAD_REQUEST: OpenApiResponse(
-                description='잘못된 페이지 또는 사이즈 요청',
-                response={
-                    'type': 'object',
-                    'properties': {
-                        'message': {
-                            'type': 'string'
-                        },
-                    },
-                }
-            ),
-            status.HTTP_401_UNAUTHORIZED: OpenApiResponse(
-                description='인증 실패',
-                response={
-                    'type': 'object',
-                    'properties': {
-                        'message': {
-                            'type': 'string'
-                        },
-                    },
-                }
-            )
-        }
-    ),
-    post=extend_schema(
-        summary='질문 등록',
-        request=QuestionCreateSerializer,
-        responses={
-            status.HTTP_201_CREATED: OpenApiResponse(
-                description='질문 등록 완료',
-                response={
-                    'type': 'object',
-                    'properties': {
-                        'message': {
-                            'type': 'string'
-                        },
-                    }
-                }
-            ),
-            status.HTTP_400_BAD_REQUEST: OpenApiResponse(
-                description='잘못된 요청',
-                response={
-                    'type': 'object',
-                    'properties': {
-                        'message': {
-                            'type': 'string'
-                        },
-                    },
-                }
-            ),
-            status.HTTP_401_UNAUTHORIZED: OpenApiResponse(
-                description='인증 실패',
-                response={
-                    'type': 'object',
-                    'properties': {
-                        'message': {
-                            'type': 'string'
-                        },
-                    },
-                }
-            )
-        }
-    )
-)
+@question_schema
 @api_view(['POST', 'GET'])
 def question(request):
     user = find_user_by_token(request)
@@ -246,95 +93,7 @@ def question(request):
         return Response(data, status=status.HTTP_200_OK)
 
 
-@extend_schema_view(
-    get=extend_schema(
-        summary='질문 조회',
-        responses={
-            status.HTTP_200_OK: OpenApiResponse(
-                description='질문 조회 성공',
-                response=QuestionSerializer
-            ),
-            status.HTTP_404_NOT_FOUND: OpenApiResponse(
-                description='질문 미존재',
-                response={
-                    'type': 'object',
-                    'properties': {
-                        'message': {
-                            'type': 'string'
-                        },
-                    }
-                }
-            )
-        }
-    ),
-    put=extend_schema(
-        summary='질문 수정',
-        request=QuestionEditSerializer,
-        responses={
-            status.HTTP_200_OK: OpenApiResponse(
-                description='질문 수정 완료',
-                response={
-                    'type': 'object',
-                    'properties': {
-                        'message': {
-                            'type': 'string'
-                        },
-                    }
-                }
-            ),
-            status.HTTP_400_BAD_REQUEST: OpenApiResponse(
-                description='잘못된 요청',
-                response={
-                    'type': 'object',
-                    'properties': {
-                        'message': {
-                            'type': 'string'
-                        },
-                    },
-                }
-            ),
-            status.HTTP_404_NOT_FOUND: OpenApiResponse(
-                description='질문 미존재',
-                response={
-                    'type': 'object',
-                    'properties': {
-                        'message': {
-                            'type': 'string'
-                        },
-                    }
-                }
-            )
-        }
-    ),
-    delete=extend_schema(
-        summary='질문 삭제',
-        responses={
-            status.HTTP_204_NO_CONTENT: OpenApiResponse(description='질문 삭제 성공'),
-            status.HTTP_400_BAD_REQUEST: OpenApiResponse(
-                description='잘못된 요청',
-                response={
-                    'type': 'object',
-                    'properties': {
-                        'message': {
-                            'type': 'string'
-                        },
-                    },
-                }
-            ),
-            status.HTTP_404_NOT_FOUND: OpenApiResponse(
-                description='질문 미존재',
-                response={
-                    'type': 'object',
-                    'properties': {
-                        'message': {
-                            'type': 'string'
-                        },
-                    }
-                }
-            )
-        }
-    )
-)
+@question_detail_schema
 @api_view(['GET', 'PUT', 'DELETE'])
 def question_detail(request, question_id):
     user_id = request.auth['user_id']
@@ -360,34 +119,7 @@ def question_detail(request, question_id):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-@extend_schema_view(
-    get=extend_schema(
-        summary='자주 묻는 질문 목록 조회',
-        responses={
-            status.HTTP_200_OK: OpenApiResponse(
-                description='자주 묻는 질문 목록 조회 성공',
-                response=FaqListSerializer(many=True),
-            )
-        }
-    ),
-    post=extend_schema(
-        summary='자주 묻는 질문 등록',
-        request=FaqCreateSerializer,
-        responses={
-            status.HTTP_201_CREATED: OpenApiResponse(
-                description='자주 묻는 질문 등록 완료',
-                response={
-                    'type': 'object',
-                    'properties': {
-                        'message': {
-                            'type': 'string'
-                        },
-                    }
-                }
-            )
-        }
-    )
-)
+@faq_schema
 @api_view(['GET', 'POST'])
 @permission_classes([AllowAny])
 def faq(request):
@@ -403,45 +135,7 @@ def faq(request):
             return Response({'message': '자주 묻는 질문이 등록되었습니다.'}, status=status.HTTP_201_CREATED)
 
 
-@extend_schema_view(
-    get=extend_schema(
-        summary='자주 묻는 질문 조회',
-        responses={
-            status.HTTP_200_OK: OpenApiResponse(
-                description='자주 묻는 질문 조회 성공',
-                response=FaqSerializer
-            ),
-            status.HTTP_404_NOT_FOUND: OpenApiResponse(
-                description='자주 묻는 질문 미존재',
-                response={
-                    'type': 'object',
-                    'properties': {
-                        'message': {
-                            'type': 'string'
-                        },
-                    }
-                }
-            )
-        }
-    ),
-    delete=extend_schema(
-        summary='자주 묻는 질문 삭제',
-        responses={
-            status.HTTP_204_NO_CONTENT: OpenApiResponse(description='자주 묻는 질문 삭제 성공'),
-            status.HTTP_404_NOT_FOUND: OpenApiResponse(
-                description='자주 묻는 질문 미존재',
-                response={
-                    'type': 'object',
-                    'properties': {
-                        'message': {
-                            'type': 'string'
-                        },
-                    }
-                }
-            )
-        }
-    )
-)
+@faq_detail_schema
 @api_view(['GET', 'DELETE'])
 @permission_classes([AllowAny])
 def faq_detail(request, faq_id):
---
+++ backend/takers/swagger_schemas.py
@@ -0,0 +1,366 @@
+from drf_spectacular.utils import OpenApiParameter, OpenApiResponse, OpenApiRequest, extend_schema_view, extend_schema
+from rest_framework import status
+
+from .serializers import TakerSerializer
+
+add_taker_schema = extend_schema_view(
+    post=extend_schema(
+        summary='응시자 등록',
+        request=TakerSerializer,
+        responses={
+            status.HTTP_201_CREATED: OpenApiResponse(
+                description='응시자 등록 성공',
+                response={
+                    'type': 'object',
+                    'properties': {
+                        'access': {
+                            'type': 'string',
+                        }
+                    }
+                }
+            ),
+            status.HTTP_400_BAD_REQUEST: OpenApiResponse(
+                description='잘못된 요청',
+                response={
+                    'type': 'object',
+                    'properties': {
+                        'message': {
+                            'type': 'string'
+                        },
+                    },
+                }
+            ),
+            status.HTTP_403_FORBIDDEN: OpenApiResponse(
+                description='시험 종료 후 요청',
+                response={
+                    'type': 'object',
+                    'properties': {
+                        'message': {
+                            'type': 'string'
+                        },
+                    },
+                }
+            ),
+            status.HTTP_429_TOO_MANY_REQUESTS: OpenApiResponse(
+                description='시험 응시 인원 초과',
+                response={
+                    'type': 'object',
+                    'properties': {
+                        'message': {
+                            'type': 'string'
+                        },
+                    },
+                }
+            )
+        }
+    ),
+    patch=extend_schema(
+        summary='시험 종료',
+        request=OpenApiRequest({
+            'type': 'object',
+            'properties': {
+                'exit_time': {
+                    'type': 'string',
+                    'format': 'date-time'
+                }
+            }
+        }),
+        responses={
+            status.HTTP_200_OK: OpenApiResponse(description='시험 종료 성공'),
+            status.HTTP_400_BAD_REQUEST: OpenApiResponse(
+                description='잘못된 요청',
+                response={
+                    'type': 'object',
+                    'properties': {
+                        'message': {
+                            'type': 'string'
+                        },
+                    },
+                }
+            ),
+            status.HTTP_409_CONFLICT: OpenApiResponse(
+                description='퇴실 불가능',
+                response={
+                    'type': 'object',
+                    'properties': {
+                        'message': {
+                            'type': 'string'
+                        },
+                    },
+                }
+            ),
+        }
+    )
+)
+
+check_email_schema = extend_schema_view(
+    get=extend_schema(
+        summary='이메일 중복 체크',
+        parameters=[
+            OpenApiParameter(name='id', type=str, location=OpenApiParameter.QUERY, required=True),
+            OpenApiParameter(name='email', type=str, location=OpenApiParameter.QUERY, required=True)
+        ],
+        responses={
+            status.HTTP_200_OK: OpenApiResponse(
+                description='이메일 중복체크 결과',
+                response={
+                    'type': 'object',
+                    'properties': {
+                        'isAlreadyExists': {
+                            'type': 'boolean',
+                        }
+                    }
+                }
+            ),
+            status.HTTP_400_BAD_REQUEST: OpenApiResponse(
+                description='잘못된 이메일 형식 또는 시험 ID, 이메일 미제공',
+                response={
+                    'type': 'object',
+                    'properties': {
+                        'message': {
+                            'type': 'string'
+                        },
+                    },
+                }
+            ),
+            status.HTTP_404_NOT_FOUND: OpenApiResponse(
+                description='존재하지 않는 시험',
+                response={
+                    'type': 'object',
+                    'properties': {
+                        'message': {
+                            'type': 'string'
+                        },
+                    },
+                }
+            )
+        }
+    ),
+    post=extend_schema(
+        summary='이메일 인증번호 발송',
+        request=OpenApiRequest({
+            'type': 'object',
+            'properties': {
+                'email': {
+                    'type': 'string',
+                }
+            }
+        }),
+        responses={
+            status.HTTP_200_OK: OpenApiResponse(
+                description='인증번호 발송 성공',
+                response={
+                    'type': 'object',
+                    'properties': {
+                        'message': {
+                            'type': 'string'
+                        },
+                    }
+                }
+            ),
+            status.HTTP_400_BAD_REQUEST: OpenApiResponse(
+                description='잘못된 이메일 형식 또는 이메일 미제공',
+                response={
+                    'type': 'object',
+                    'properties': {
+                        'message': {
+                            'type': 'string'
+                        },
+                    },
+                }
+            ),
+        }
+    ),
+    put=extend_schema(
+        summary='이메일 인증',
+        request=OpenApiRequest({
+            'type': 'object',
+            'properties': {
+                'email': {
+                    'type': 'string',
+                },
+                'code': {
+                    'type': 'string',
+                }
+            }
+        }),
+        responses={
+            status.HTTP_200_OK: OpenApiResponse(
+                description='인증번호 인증 완료',
+                response={
+                    'type': 'object',
+                    'properties': {
+                        'message': {
+                            'type': 'string'
+                        },
+                    }
+                }
+            ),
+            status.HTTP_400_BAD_REQUEST: OpenApiResponse(
+                description='잘못된 인증번호 또는 만료된 인증번호',
+                response={
+                    'type': 'object',
+                    'properties': {
+                        'message': {
+                            'type': 'string'
+                        },
+                    },
+                }
+            ),
+        }
+    )
+)
+
+update_taker_schema = extend_schema_view(
+    patch=extend_schema(
+        summary='신분증 등록',
+        request=OpenApiRequest({
+            'type': 'object',
+            'properties': {
+                'id_photo': {
+                    'type': 'file'
+                },
+                'birth': {
+                    'type': 'string',
+                },
+                'verification_rate': {
+                    'type': 'integer',
+                }
+            }
+        }),
+        responses={
+            status.HTTP_200_OK: OpenApiResponse(
+                description='응시자 정보 업데이트 성공',
+                response={
+                    'type': 'object',
+                    'properties': {
+                        'message': {
+                            'type': 'string'
+                        },
+                    }
+                }
+            ),
+            status.HTTP_400_BAD_REQUEST: OpenApiResponse(
+                description='잘못된 요청',
+                response={
+                    'type': 'object',
+                    'properties': {
+                        'message': {
+                            'type': 'string'
+                        },
+                    },
+                }
+            ),
+            status.HTTP_401_UNAUTHORIZED: OpenApiResponse(
+                description='인증 실패',
+                response={
+                    'type': 'object',
+                    'properties': {
+                        'message': {
+                            'type': 'string'
+                        },
+                    },
+                }
+            ),
+            status.HTTP_403_FORBIDDEN: OpenApiResponse(
+                description='해당하는 role 사용자가 아님',
+                response={
+                    'type': 'object',
+                    'properties': {
+                        'message': {
+                            'type': 'string'
+                        },
+                    },
+                }
+            ),
+            status.HTTP_404_NOT_FOUND: OpenApiResponse(
+                description='존재하지 않는 응시자',
+                response={
+                    'type': 'object',
+                    'properties': {
+                        'message': {
+                            'type': 'string'
+                        },
+                    },
+                }
+            )
+        }
+    )
+)
+
+add_web_cam_schame = extend_schema_view(
+    patch=extend_schema(
+        summary='웹캠 파일 추가',
+        request=OpenApiRequest({
+            'type': 'object',
+            'properties': {
+                'web_cam': {
+                    'type': 'file'
+                },
+                'start_time': {
+                    'type': 'string',
+                },
+                'end_time': {
+                    'type': 'string',
+                }
+            }
+        }),
+        responses={
+            status.HTTP_200_OK: OpenApiResponse(
+                description='웹캠 저장 성공',
+                response={
+                    'type': 'object',
+                    'properties': {
+                        'message': {
+                            'type': 'string'
+                        },
+                    }
+                }
+            ),
+            status.HTTP_400_BAD_REQUEST: OpenApiResponse(
+                description='잘못된 요청',
+                response={
+                    'type': 'object',
+                    'properties': {
+                        'message': {
+                            'type': 'string'
+                        },
+                    },
+                }
+            ),
+            status.HTTP_401_UNAUTHORIZED: OpenApiResponse(
+                description='인증 실패',
+                response={
+                    'type': 'object',
+                    'properties': {
+                        'message': {
+                            'type': 'string'
+                        },
+                    },
+                }
+            ),
+            status.HTTP_403_FORBIDDEN: OpenApiResponse(
+                description='해당하는 role 사용자가 아님',
+                response={
+                    'type': 'object',
+                    'properties': {
+                        'message': {
+                            'type': 'string'
+                        },
+                    },
+                }
+            ),
+            status.HTTP_404_NOT_FOUND: OpenApiResponse(
+                description='존재하지 않는 응시자',
+                response={
+                    'type': 'object',
+                    'properties': {
+                        'message': {
+                            'type': 'string'
+                        },
+                    },
+                }
+            )
+        }
+    )
+)
