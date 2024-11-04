cde926f1bb77114b6a6d9977b60739e80797c115
윤예리 <yoonr72@naver.com>
2024-11-04 10:59:07 +0900

    [#S11P31S209-338] [FE] :sparkles: feat: 공지 조회 api 추가

--- frontend/src/pages/host/Helpdesk.tsx
+++ frontend/src/pages/host/Helpdesk.tsx
@@ -5,23 +5,73 @@ import Textfield from "@/components/Textfield";
 import { useEffect, useState } from "react";
 import HostHeader from "@/components/HostHeader";
 import axiosInstance from "@/utils/axios";
+
+interface FAQ {
+  id: number;
+  title: string;
+}
+
+interface notification {
+  id: number;
+  title: string;
+  category: "공지사항";
+  created_at: string;
+}
+
+interface question {
+  Id: number;
+  organizer: string;
+  category: string;
+  title: string;
+  created_at: string;
+  updated_at: string;
+}
+
 const Helpdesk = () => {
   const categories = ["전체", "공지사항", "이용 방법", "적립금", "기타"];
   const [currentCategory, setCurrentCategory] = useState("전체");
-  const [faqs, setFaqs] = useState([]);
-  const [tableData, setTableData] = useState([]);
+  const [faqs, setFaqs] = useState<FAQ[]>([]);
+  const [notifications, setNotifications] = useState<notification[]>([]);
+  const [questions, setQuestions] = useState<question[]>([]);
 
-  useEffect(() => {
+  const fetchFAQ = () => {
     axiosInstance
       .get("/helpdesk/faq/")
       .then((response) => {
-        console.log(response.data);
-        setFaqs(response.data);
+        setFaqs(response.data.faqList);
       })
       .catch((error) => {
-        console.error("실패:", error);
+        console.error("FAQ 조회 실패:", error);
       });
-  }, [faqs]);
+  };
+
+  const fetchQuestions = () => {
+    axiosInstance
+      .get("/helpdesk/question/")
+      .then((response) => {
+        setQuestions(response.data.questionList);
+      })
+      .catch((error) => {
+        console.error("질문 조회 실패:", error);
+      });
+  };
+
+  const fetchNotifications = () => {
+    axiosInstance
+      .get("/helpdesk/notification/")
+      .then((response) => {
+        setNotifications(response.data.notificationList);
+      })
+      .catch((error) => {
+        console.error("공지 조회 실패:", error);
+      });
+  };
+
+  useEffect(() => {
+    fetchFAQ();
+    fetchQuestions();
+    fetchNotifications();
+  }, []);
 
   const handleCategoryClick = (category: string) => {
     setCurrentCategory(category);
@@ -30,7 +80,7 @@ const Helpdesk = () => {
   return (
     <>
       <HostHeader />
-      {/* faq */}
+      {/* FAQ Section */}
       <div className={styles.faqContainer}>
         <div className={styles.title}>
           <span style={fonts.HEADING_LG_BOLD}>자주 묻는 질문(FAQ)</span>
@@ -48,7 +98,7 @@ const Helpdesk = () => {
 
       <div className={styles.separator}></div>
 
-      {/* manual */}
+      {/* Manual Section */}
       <div className={styles.manualContainer}>
         <div className={styles.title}>
           <span style={fonts.HEADING_LG_BOLD}>프록토매틱 매뉴얼</span>
@@ -60,11 +110,13 @@ const Helpdesk = () => {
         </div>
       </div>
 
+      {/* Question & Notification Section */}
       <div className={styles.questionContainer}>
         <div className={styles.questionWrap}>
           <div className={styles.questionTitle}>
             <span style={fonts.HEADING_MD_BOLD}>공지사항 & 질문</span>
           </div>
+
           <div className={styles.questionContent}>
             {/* 헤더 */}
             <div className={styles.questionHeader}>
@@ -101,11 +153,11 @@ const Helpdesk = () => {
                 </CustomButton>
               </div>
             </div>
-            {/* 표 */}
+
+            {/* Questions & Notifications Table */}
             <div className={styles.questionTable}>
               <div>
                 <div className={styles.tableSeparator}></div>
-
                 <table className={styles.table}>
                   <thead className={styles.tableTitle}>
                     <tr>
@@ -115,7 +167,35 @@ const Helpdesk = () => {
                     </tr>
                   </thead>
                   <tbody className={styles.tableBody}>
-                    {tableData.map((row, index) => (
+                    {notifications.map((item, index) => (
+                      <tr
+                        key={index}
+                        style={{ backgroundColor: "rgba(200, 59, 56, 0.1)" }}
+                      >
+                        <td
+                          style={{
+                            textAlign: "center",
+                            fontWeight: "bold",
+                            color: "var(--GRAY_600)",
+                          }}
+                        >
+                          공지
+                        </td>
+                        <td>
+                          <div style={{ fontWeight: "bold" }}>{item.title}</div>
+                          <div
+                            style={{
+                              color: "var(--GRAY_500)",
+                              ...fonts.MD_REGULAR,
+                            }}
+                          >
+                            {item.created_at}
+                          </div>
+                        </td>
+                        <td />
+                      </tr>
+                    ))}
+                    {questions.map((item, index) => (
                       <tr key={index}>
                         <td
                           style={{
@@ -124,15 +204,24 @@ const Helpdesk = () => {
                             color: "var(--GRAY_600)",
                           }}
                         >
-                          {row.category}
+                          {item.category}
                         </td>
                         <td>
-                          <div style={{ fontWeight: "bold" }}>{row.title}</div>
-                          <div style={{ color: "var(--GRAY_500)" }}>
-                            {row.created_at}
+                          <div style={{ fontWeight: "bold" }}>{item.title}</div>
+                          <div
+                            style={{
+                              color: "var(--GRAY_500)",
+                              ...fonts.MD_REGULAR,
+                            }}
+                          >
+                            {item.created_at}
                           </div>
                         </td>
-                        <td style={{ textAlign: "center" }}>{row.organizer}</td>
+                        <td>
+                          <div style={{ textAlign: "center" }}>
+                            {item.organizer}
+                          </div>
+                        </td>
                       </tr>
                     ))}
                   </tbody>
