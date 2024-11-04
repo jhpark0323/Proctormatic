073c7b334d16e1949b2b12a9bcf37c057e583f74
이유찬 <dakgoo02@naver.com>
2024-11-04 12:48:03 +0900

    [#S11P31S209-341] [FE] :sparkles: feat: 응시자 회원가입 중 URL 검증 및 응시자 정보 입력 API 연동

--- frontend/src/pages/taker/steps/Step4.tsx
+++ frontend/src/pages/taker/steps/Step4.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import styles from '@/styles/Step.module.css';
 import CustomButton from '@/components/CustomButton';
+import { useTakerStore } from '@/store/TakerAuthStore';
 
 const Step4: React.FC<{ onNext: () => void }> = ({ onNext }) => {
+  const { testId } = useTakerStore();
+
   return (
     <>
       <div className={styles.StepTitleBox}>
         <div className={styles.StepTitle}>시험정보 확인</div>
         <div className={styles.StepSubTitle}>응시하실 시험의 상세 정보를 확인해주세요.</div>
       </div>
-      <div className={styles.StepInner}></div>
+      <div className={styles.StepInner}>
+        <div>시험ID: { testId }</div> 
+      </div>
       <div className={styles.StepFooter}>
         <CustomButton onClick={onNext}>다음</CustomButton>
       </div>
