af9a21eec461193308fe8706b79918a4efd333ee
윤예리 <yoonr72@naver.com>
2024-11-04 17:55:39 +0900

    [#S11P31S209-361] [FE] :sparkles: feat: 시험 생성 페이지
    - 시간 필드, 날짜 필드 수정 필요
    - 예외 처리
    - 테스트 코드 필요

--- frontend/src/pages/host/testpages/MakeTest.tsx
+++ frontend/src/pages/host/testpages/MakeTest.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Textfield from "@/components/Textfield";
 import { fonts } from "@/constants";
 import styles from "@/styles/Testpage.module.css";
@@ -6,6 +6,10 @@ import { FaArrowLeft } from "react-icons/fa6";
 import Checkbox from "@/components/Checkbox";
 import CustomButton from "@/components/CustomButton";
 import HostHeader from "@/components/HostHeader";
+import { useNavigate } from "react-router-dom";
+import { useAuthStore } from "@/store/useAuthStore";
+import axiosInstance from "@/utils/axios";
+import { CustomToast } from "@/components/CustomToast";
 
 // 시간, 날짜 input
 import { LocalizationProvider } from "@mui/x-date-pickers";
@@ -13,16 +17,116 @@ import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { DatePicker, TimePicker } from "@mui/x-date-pickers";
 import { ko } from "date-fns/locale";
 
+interface TestForm {
+  title: string;
+  date: string;
+  start_time: string;
+  end_time: string;
+  exit_time?: string;
+  expected_taker: number;
+  cheer_msg?: string;
+  cost: number;
+}
+
 const MakeTest = () => {
   const [isExitPermitted, setIsExitPermitted] = useState(false);
-  const [startDate, setStartDate] = useState<Date | null>(null);
-  // const [endDate, setEndDate] = useState<Date | null>(null);
+  const navigate = useNavigate();
+
+  // 사용자 정보 조회
+  const [userEmail, setUserEmail] = useState("");
+  const { user } = useAuthStore();
+  const fetchUserEmail = () => {
+    axiosInstance
+      .get("/users/")
+      .then((response) => {
+        setUserEmail(response.data.email);
+      })
+      .catch((error) => {
+        console.log("이메일 조회 실패: ", error);
+      });
+  };
+
+  // 코인 불러오기
+  const [currentCoinAmount, setCurrentCoinAmount] = useState(0);
+  const fetchCurrentCoinAmount = () => {
+    axiosInstance
+      .get("/coin/")
+      .then((response) => {
+        setCurrentCoinAmount(response.data.coin);
+      })
+      .catch((error) => {
+        console.log("코인 조회 실패: ", error);
+      });
+  };
+
+  useEffect(() => {
+    fetchUserEmail();
+    fetchCurrentCoinAmount();
+  }, []);
+
+  // 시험 만들기
+  const [testForm, setTestForm] = useState<TestForm>({
+    title: "",
+    date: "",
+    start_time: "",
+    end_time: "",
+    exit_time: "",
+    expected_taker: 0,
+    cost: 0,
+  });
+
+  const handleDateTimeChange = (field: keyof TestForm, value: Date | null) => {
+    if (value) {
+      let formattedValue = "";
+      if (field === "date") {
+        // 날짜: "YYYY-MM-DD"
+        formattedValue = value.toISOString().split("T")[0];
+      } else {
+        // 시간: "HH:mm:ss"
+        formattedValue = value.toISOString().split("T")[1].split(".")[0];
+      }
+      setTestForm((prevForm) => ({
+        ...prevForm,
+        [field]: formattedValue,
+      }));
+    }
+  };
+
+  const submitTestForm = (form: TestForm) => {
+    if (
+      !testForm.title ||
+      !testForm.date ||
+      !testForm.start_time ||
+      !testForm.end_time ||
+      !testForm.expected_taker
+    ) {
+      CustomToast("비어있는 칸이 있습니다!");
+      return;
+    }
+
+    const completeForm = {
+      ...testForm,
+      cost: testForm.expected_taker * 60,
+      exit_time: testForm.exit_time || testForm.end_time, // exit_time이 비어있다면 end_time으로 설정
+    };
+
+    axiosInstance
+      .post("/exam/", completeForm)
+      .then((response) => {
+        console.log(response.data);
+        // navigate("/host/myTest");
+      })
+      .catch((error) => {
+        console.log(completeForm);
+        console.log("시험 생성 실패: ", error);
+      });
+  };
 
   return (
     <>
       <HostHeader />
       <div className={styles.makeTestContainer}>
-        <div className={styles.goBack}>
+        <div className={styles.goBack} onClick={() => navigate(-1)}>
           <FaArrowLeft style={{ opacity: "0.5" }} />
           <div style={fonts.HEADING_SM_BOLD}>시험 예약하기</div>
         </div>
@@ -32,16 +136,25 @@ const MakeTest = () => {
             <div className={styles.makeTestContentItem}>
               <div className={styles.makeTestContentTitle}>주최자 정보</div>
               <div className={styles.makeTestName} style={fonts.LG_SEMIBOLD}>
-                홍길동 (honggildong@gmail.com)
+                {user?.name} ({userEmail})
               </div>
             </div>
             <div className={styles.makeTestContentItem}>
-              <Textfield label="시험 제목" placeholder="시험 제목 입력" />
+              <Textfield
+                label="시험 제목"
+                placeholder="시험 제목 입력"
+                value={testForm.title}
+                onChange={(value) => setTestForm({ ...testForm, title: value })} // 제목 설정 추가
+              />
             </div>
             <div className={styles.makeTestContentItem}>
               <Textfield
                 label="응원 메세지 (선택)"
                 placeholder="응원 메세지 입력 (최대 100자)"
+                value={testForm.cheer_msg}
+                onChange={(value) =>
+                  setTestForm({ ...testForm, cheer_msg: value })
+                }
               />
             </div>
             <div className={styles.makeTestContentItem}>
@@ -61,10 +174,16 @@ const MakeTest = () => {
                     dateAdapter={AdapterDateFns}
                     adapterLocale={ko}
                   >
-                    <div style={{ display: "flex", gap: "1rem" }}>
+                    <div style={{ display: "flex", gap: "0.5rem" }}>
                       <DatePicker
-                        value={startDate}
-                        onChange={(newValue) => setStartDate(newValue)}
+                        value={
+                          testForm.start_time
+                            ? new Date(`1970-01-01T${testForm.start_time}`)
+                            : null
+                        }
+                        onChange={(newValue) =>
+                          handleDateTimeChange("start_time", newValue)
+                        }
                         slotProps={{
                           textField: {
                             helperText: null,
@@ -75,8 +194,14 @@ const MakeTest = () => {
                         }}
                       />
                       <TimePicker
-                        value={startDate}
-                        onChange={(newValue) => setStartDate(newValue)}
+                        value={
+                          testForm.start_time
+                            ? new Date(`1970-01-01T${testForm.start_time}`)
+                            : null
+                        }
+                        onChange={(newValue) =>
+                          handleDateTimeChange("start_time", newValue)
+                        }
                         slotProps={{
                           textField: {
                             helperText: null,
@@ -89,6 +214,7 @@ const MakeTest = () => {
                     </div>
                   </LocalizationProvider>
                 </div>
+
                 <div className={styles.endTimeWrap}>
                   <div className={styles.timeInput}>
                     <div>응시 종료:</div>
@@ -96,10 +222,12 @@ const MakeTest = () => {
                       dateAdapter={AdapterDateFns}
                       adapterLocale={ko}
                     >
-                      <div style={{ display: "flex", gap: "1rem" }}>
+                      <div style={{ display: "flex", gap: "0.5rem" }}>
                         <DatePicker
-                          value={startDate}
-                          onChange={(newValue) => setStartDate(newValue)}
+                          value={testForm.date ? new Date(testForm.date) : null}
+                          onChange={(newValue) =>
+                            handleDateTimeChange("date", newValue)
+                          }
                           slotProps={{
                             textField: {
                               helperText: null,
@@ -113,8 +241,14 @@ const MakeTest = () => {
                           }}
                         />
                         <TimePicker
-                          value={startDate}
-                          onChange={(newValue) => setStartDate(newValue)}
+                          value={
+                            testForm.end_time
+                              ? new Date(`1970-01-01T${testForm.end_time}`)
+                              : null
+                          }
+                          onChange={(newValue) =>
+                            handleDateTimeChange("end_time", newValue)
+                          }
                           slotProps={{
                             textField: {
                               helperText: null,
@@ -143,10 +277,14 @@ const MakeTest = () => {
                         dateAdapter={AdapterDateFns}
                         adapterLocale={ko}
                       >
-                        <div style={{ display: "flex", gap: "1rem" }}>
+                        <div style={{ display: "flex", gap: "0.5rem" }}>
                           <DatePicker
-                            value={startDate}
-                            onChange={(newValue) => setStartDate(newValue)}
+                            value={
+                              testForm.date ? new Date(testForm.date) : null
+                            }
+                            onChange={(newValue) =>
+                              handleDateTimeChange("date", newValue)
+                            }
                             slotProps={{
                               textField: {
                                 helperText: null,
@@ -160,8 +298,14 @@ const MakeTest = () => {
                             }}
                           />
                           <TimePicker
-                            value={startDate}
-                            onChange={(newValue) => setStartDate(newValue)}
+                            value={
+                              testForm.end_time
+                                ? new Date(`1970-01-01T${testForm.end_time}`)
+                                : null
+                            }
+                            onChange={(newValue) =>
+                              handleDateTimeChange("end_time", newValue)
+                            }
                             slotProps={{
                               textField: {
                                 helperText: null,
@@ -225,8 +369,18 @@ const MakeTest = () => {
                     alignItems: "center",
                   }}
                 >
-                  <div style={{ width: "5%" }}>
-                    <Textfield placeholder="0" maxLength={3} />
+                  <div style={{ width: "15%" }}>
+                    <Textfield
+                      placeholder="0"
+                      maxLength={3}
+                      value={testForm.expected_taker.toString()}
+                      onChange={(value) =>
+                        setTestForm({
+                          ...testForm,
+                          expected_taker: parseInt(value) || 0,
+                        })
+                      }
+                    />
                   </div>
                   <div className={styles.makeTestContentTitle}>명</div>
                 </div>
@@ -235,12 +389,13 @@ const MakeTest = () => {
                     className={styles.coinAmount}
                     style={fonts.HEADING_MD_BOLD}
                   >
-                    600
+                    {testForm.expected_taker * 60}
                   </div>
                   <div className={styles.makeTestContentTitle}>C</div>
                 </div>
               </div>
             </div>
+
             <div
               className={styles.makeTestContentItem}
               style={{ display: "flex", justifyContent: "space-between" }}
@@ -256,7 +411,7 @@ const MakeTest = () => {
                   className={styles.coinAmount}
                   style={fonts.HEADING_MD_BOLD}
                 >
-                  600
+                  {currentCoinAmount}
                 </div>
                 <div className={styles.makeTestContentTitle}>C</div>
               </div>
@@ -265,7 +420,7 @@ const MakeTest = () => {
         </div>
 
         <div className={styles.submitButton}>
-          <CustomButton>
+          <CustomButton onClick={() => submitTestForm(testForm)}>
             <span style={fonts.MD_SEMIBOLD}>결제 및 예약하기</span>
           </CustomButton>
         </div>
