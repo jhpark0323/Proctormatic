3d162acce9b0aad44a68d8d2d0ef826992cecfbb
윤예리 <yoonr72@naver.com>
2024-11-12 20:58:04 +0900

    [#S11P31S209-489] [FE] fix: model 경로 수정

--- frontend/src/pages/taker/steps/Step8.tsx
+++ frontend/src/pages/taker/steps/Step8.tsx
@@ -42,7 +42,7 @@ const Step8: React.FC<{ onNext: () => void }> = ({ onNext }) => {
   };
 
   const loadModels = async () => {
-    const MODEL_URL = "/dist/models";
+    const MODEL_URL = `${process.env.PUBLIC_URL}/dist/models`;
     await faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL);
     setModelsLoaded(true);
   };
