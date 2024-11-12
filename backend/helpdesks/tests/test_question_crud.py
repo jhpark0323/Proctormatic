b598934b87dc3158cbc60c1382d2c54cc4ce2b46
허유정 <yu.jeong5899@gmail.com>
2024-11-13 02:26:43 +0900

    [#S11P31S209-438] [BE] test: 답변 CRUD 테스트 코드

--- backend/helpdesks/tests/test_question_crud.py
+++ backend/helpdesks/tests/test_question_crud.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework_simplejwt.tokens import AccessToken
 from helpdesks.models import Question
-from helpdesks.views import question
+
 
 User = get_user_model()
 
