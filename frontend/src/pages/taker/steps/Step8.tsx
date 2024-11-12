077576ec5b2abe634bbae346d917b60928bfa4dd
윤예리 <yoonr72@naver.com>
2024-11-13 08:58:26 +0900

    [#S11P31S209-489] [FE] feat: 빌드 경로 수정

--- frontend/src/pages/taker/steps/Step8.tsx
+++ frontend/src/pages/taker/steps/Step8.tsx
@@ -42,7 +42,7 @@ const Step8: React.FC<{ onNext: () => void }> = ({ onNext }) => {
   };
 
   const loadModels = async () => {
-    const MODEL_URL = "https://k11s209.p.ssafy.io/dist/models";
+    const MODEL_URL = "/models";
     await faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL);
     setModelsLoaded(true);
   };
