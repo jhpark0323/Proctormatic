e5e6c7b70a4c130a15c1a1c0c1e536e7a0573de8
윤예리 <yoonr72@naver.com>
2024-11-17 20:25:19 +0900

    [#S11P31S209-517] [FE] :sparkles: feat: 모자이크 추가

--- frontend/src/hooks/mediapipeHooks/useFaceBoundingBox.ts
+++ frontend/src/hooks/mediapipeHooks/useFaceBoundingBox.ts
@@ -1,14 +1,15 @@
-const useFaceBoundingBox = (landmarks: any) => {
+export const useFaceBoundingBox = (
+  landmarks: any,
+  canvas: HTMLCanvasElement
+) => {
   const getBoundingBox = () => {
-    const xCoords = landmarks.map((point: any) => point.x);
-    const yCoords = landmarks.map((point: any) => point.y);
-    const minX = Math.min(...xCoords) * 800;
-    const minY = Math.min(...yCoords) * 600;
-    const maxX = Math.max(...xCoords) * 800;
-    const maxY = Math.max(...yCoords) * 600;
+    const xCoords = landmarks.map((point: any) => point.x * canvas.width);
+    const yCoords = landmarks.map((point: any) => point.y * canvas.height);
+    const minX = Math.min(...xCoords);
+    const minY = Math.min(...yCoords);
+    const maxX = Math.max(...xCoords);
+    const maxY = Math.max(...yCoords);
     return { x: minX, y: minY, width: maxX - minX, height: maxY - minY };
   };
   return getBoundingBox();
 };
-
-export default useFaceBoundingBox;
