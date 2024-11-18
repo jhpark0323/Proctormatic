85800e9b4964d195a624a6020c57406c253b303e
이유찬 <dakgoo02@naver.com>
2024-11-18 11:51:59 +0900

    [#S11P31S209-531] [FE] :bug: fix: 영상 업로드 전일때는 업로드 전이라는 문구 출력

--- frontend/src/pages/host/testpages/ReportDetail.tsx
+++ frontend/src/pages/host/testpages/ReportDetail.tsx
@@ -13,7 +13,7 @@ interface Taker {
   email: string;
   birth: string;
   id_photo: string;
-  web_cam: string;
+  web_cam: string | null;
   verification_rate: number;
   date: string;
   entry_time: string;
@@ -35,8 +35,8 @@ const ReportDetail = ({}: ReportDetailProps) => {
     name: "",
     email: "",
     birth: "",
-    id_photo: "", // 기본 이미지 추가 필요
-    web_cam: "", // 기본 영상 추가 필요
+    id_photo: "",
+    web_cam: null,
     verification_rate: 0,
     date: "",
     entry_time: "",
@@ -56,7 +56,7 @@ const ReportDetail = ({}: ReportDetailProps) => {
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+  }, [eid, tid]);
 
   const navigate = useNavigate();
   return (
@@ -75,52 +75,30 @@ const ReportDetail = ({}: ReportDetailProps) => {
             {/* 응시자 정보 */}
             <div className={styles.detailInfoItem}>
               <div className={styles.reportItem}>
-                <div
-                  className={styles.detailInfoTitle}
-                  style={fonts.HEADING_SM_BOLD}
-                >
+                <div className={styles.detailInfoTitle} style={fonts.HEADING_SM_BOLD}>
                   응시자 이름
                 </div>
-                <div className={styles.detailInfoContext}>
-                  {takerData?.name}
-                </div>
+                <div className={styles.detailInfoContext}>{takerData?.name}</div>
               </div>
 
               <div className={styles.reportItem}>
-                <div
-                  className={styles.detailInfoTitle}
-                  style={fonts.HEADING_SM_BOLD}
-                >
+                <div className={styles.detailInfoTitle} style={fonts.HEADING_SM_BOLD}>
                   응시자 이메일
                 </div>
-                <div className={styles.detailInfoContext}>
-                  {takerData?.email}
-                </div>
+                <div className={styles.detailInfoContext}>{takerData?.email}</div>
               </div>
               <div className={styles.reportItem}>
-                <div
-                  className={styles.detailInfoTitle}
-                  style={fonts.HEADING_SM_BOLD}
-                >
+                <div className={styles.detailInfoTitle} style={fonts.HEADING_SM_BOLD}>
                   응시자 생년월일
                 </div>
-                <div className={styles.detailInfoContext}>
-                  {takerData?.birth}
-                </div>
+                <div className={styles.detailInfoContext}>{takerData?.birth}</div>
               </div>
               <div className={styles.reportItem}>
-                <div
-                  className={styles.detailInfoTitle}
-                  style={fonts.HEADING_SM_BOLD}
-                >
+                <div className={styles.detailInfoTitle} style={fonts.HEADING_SM_BOLD}>
                   신분증 사진
                 </div>
                 <div className={styles.detailInfoContext}>
-                  <img
-                    src={takerData?.id_photo}
-                    alt="신분증 사진"
-                    className={styles.takerIdImage}
-                  />
+                  <img src={takerData?.id_photo} alt="신분증 사진" className={styles.takerIdImage} />
                 </div>
               </div>
             </div>
@@ -128,69 +106,42 @@ const ReportDetail = ({}: ReportDetailProps) => {
             {/* 시험 정보 */}
             <div className={styles.detailInfoItem}>
               <div className={styles.reportItem}>
-                <div
-                  className={styles.detailInfoTitle}
-                  style={fonts.HEADING_SM_BOLD}
-                >
+                <div className={styles.detailInfoTitle} style={fonts.HEADING_SM_BOLD}>
                   시험 날짜
                 </div>
-                <div className={styles.detailInfoContext}>
-                  {takerData?.date}
-                </div>
+                <div className={styles.detailInfoContext}>{takerData?.date}</div>
               </div>
               <div className={styles.reportItem}>
-                <div
-                  className={styles.detailInfoTitle}
-                  style={fonts.HEADING_SM_BOLD}
-                >
+                <div className={styles.detailInfoTitle} style={fonts.HEADING_SM_BOLD}>
                   입장 시간
                 </div>
                 <div className={styles.detailInfoContext}>
-                  {takerData?.entry_time
-                    ? takerData.entry_time.slice(0, 8)
-                    : "정보 없음"}
+                  {takerData?.entry_time ? takerData.entry_time.slice(0, 8) : "정보 없음"}
                 </div>
               </div>
               <div className={styles.reportItem}>
-                <div
-                  className={styles.detailInfoTitle}
-                  style={fonts.HEADING_SM_BOLD}
-                >
+                <div className={styles.detailInfoTitle} style={fonts.HEADING_SM_BOLD}>
                   퇴장 시간
                 </div>
                 <div className={styles.detailInfoContext}>
-                  {takerData?.exit_time
-                    ? takerData.exit_time.slice(0, 8)
-                    : "정보 없음"}
+                  {takerData?.exit_time ? takerData.exit_time.slice(0, 8) : "정보 없음"}
                 </div>
               </div>
               <div className={styles.reportItem}>
-                <div
-                  className={styles.detailInfoTitle}
-                  style={fonts.HEADING_SM_BOLD}
-                >
+                <div className={styles.detailInfoTitle} style={fonts.HEADING_SM_BOLD}>
                   재입실 횟수
                 </div>
-                <div className={styles.detailInfoContext}>
-                  {takerData?.number_of_entry}
-                </div>
+                <div className={styles.detailInfoContext}>{takerData?.number_of_entry}</div>
               </div>
               <div className={styles.reportItem}>
-                <div
-                  className={styles.detailInfoTitle}
-                  style={fonts.HEADING_SM_BOLD}
-                >
+                <div className={styles.detailInfoTitle} style={fonts.HEADING_SM_BOLD}>
                   정상퇴실 여부
                 </div>
                 <div
                   className={styles.detailInfoContext}
                   style={{
-                    color:
-                      takerData?.check_out_state === "normal"
-                        ? ""
-                        : "var(--DELETE)",
-                    fontWeight:
-                      takerData?.check_out_state === "abnormal" ? "bold" : "",
+                    color: takerData?.check_out_state === "normal" ? "" : "var(--DELETE)",
+                    fontWeight: takerData?.check_out_state === "abnormal" ? "bold" : "",
                   }}
                 >
                   {takerData?.check_out_state === "normal" ? "정상" : "비정상"}
@@ -200,30 +151,28 @@ const ReportDetail = ({}: ReportDetailProps) => {
 
             {/* 영상 */}
             <div className={styles.reportItem}>
-              <div
-                className={styles.detailInfoTitle}
-                style={fonts.HEADING_SM_BOLD}
-              >
+              <div className={styles.detailInfoTitle} style={fonts.HEADING_SM_BOLD}>
                 응시 영상
               </div>
               <div className={styles.detailInfoContext}>
-                <video
-                  id="videoPlayer"
-                  controls
-                  src={takerData?.web_cam}
-                  className={styles.takerVideo}
-                >
-                  현재 브라우저는 동영상을 지원하지 않습니다.
-                </video>
+                {takerData?.web_cam ? (
+                  <video
+                    id="videoPlayer"
+                    controls
+                    src={takerData.web_cam}
+                    className={styles.takerVideo}
+                  >
+                    현재 브라우저는 동영상을 지원하지 않습니다.
+                  </video>
+                ) : (
+                  "영상 업로드 중입니다..."
+                )}
               </div>
             </div>
 
             {/* 타임라인 버튼 */}
             <div className={styles.detailEventItem}>
-              <div
-                className={styles.detailInfoTitle}
-                style={fonts.HEADING_SM_BOLD}
-              >
+              <div className={styles.detailInfoTitle} style={fonts.HEADING_SM_BOLD}>
                 감지된 이벤트 타임라인
               </div>
               <div className={styles.timelineButtons}>
@@ -250,17 +199,13 @@ const ReportDetail = ({}: ReportDetailProps) => {
                         color: borderColor,
                       }}
                       onClick={() => {
-                        const videoElement = document.getElementById(
-                          "videoPlayer"
-                        ) as HTMLVideoElement;
+                        const videoElement = document.getElementById("videoPlayer") as HTMLVideoElement;
                         if (videoElement) {
                           const timeParts = event.detected_time
                             .split(":")
                             .map((part) => parseFloat(part));
                           const timeInSeconds = timeParts.reduce(
-                            (acc, time, i) =>
-                              acc +
-                              time * Math.pow(60, timeParts.length - 1 - i),
+                            (acc, time, i) => acc + time * Math.pow(60, timeParts.length - 1 - i),
                             0
                           );
                           videoElement.currentTime = timeInSeconds;
